4c8f9639bbd6a1c8fdc0fb99b47ee3314bb001ab
Oleksandr <dr.newman.ua@gmail.com>
2026-01-12 20:37:34 +0200

    Revert "preparation for next exercises"
    This reverts commit c813bd49dc8ccc87090a25838f2f37c611bfa9a8.

--- src/main/java/ucu/ddb/practice/ClientController.java
+++ src/main/java/ucu/ddb/practice/ClientController.java
@@ -31,8 +31,8 @@ public class ClientController {
     @GetMapping("/run")
     public String runTest(@RequestParam String mode, @RequestParam int count, @RequestParam int clients) {
         return switch (mode) {
-            case "ex1_part1" -> run(count, clients, "inc_p1", "count_p1", 1, 1);
-            case "ex1_part2" -> run(count, clients, "inc_p2", "count_p2", 1, 2);
+            case "ex1_part1" -> runEx1(count, clients, "inc_p1", "count_p1");
+            case "ex1_part2" -> runEx1(count, clients, "inc_p2", "count_p2");
             default -> """
                     Режим виконання заданий не правильно.
                     Має складатись з ex(номер завдання)_part(номер пункту завдання).
@@ -43,23 +43,22 @@ public class ClientController {
 
     @GetMapping("/run_ex1_part1")
     public String runTestEx1Part1Cli1(@RequestParam int clients) {
-        return run(10000, clients, "inc_p1", "count_p1", 1, 1);
+        return runEx1(10000, clients, "inc_p1", "count_p1");
     }
 
     @GetMapping("/run_ex1_part2")
     public String runTestEx1Part1Cli2(@RequestParam int clients) {
-        return run(10000, clients, "inc_p2", "count_p2", 1, 2);
+        return runEx1(10000, clients, "inc_p2", "count_p2");
     }
 
-    private String run(int count, int clients, String incCommand, String countCommand,
-                       int exerciseNumber, int partNumber) {
+    private String runEx1(int count, int clients, String incCommand, String countCommand) {
         StringBuilder result = new StringBuilder(String.format("""
-            Виконання тесту за логікою завдання %d, пункт %d з параметрами:
+            Виконання тесту за логікою завдання 1, пункт 1 з параметрами:
              - кількість викликів = %d
              - кількість клієнтів = %d
             
             Результати:
-            """, exerciseNumber, partNumber, count, clients));
+            """, count, clients));
         log.info(result.toString());
 
         CountDownLatch latch = new CountDownLatch(clients);
